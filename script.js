67a1486624c8c92fc703fbf01b615ff9f56aa0dc
Ego Lydia <94656569+EgoLydia@users.noreply.github.com>
2022-02-27 18:52:56 +0100

    Feature: Add Array

---
+++ script.js
@@ -0,0 +1,9 @@
+var images = [
+    "img/longexposurewaterfall1.jpg",
+    "img/archesdrive.jpg",
+    "img/army2.jpg",
+    "img/redwoodupwards2.jpg",
+    "img/route66.jpg",
+    "img/solduc2.jpg",
+    "img/truck4scale.jpg",
+ ]
