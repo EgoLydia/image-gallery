862bdac7f843f066ec7cc9d6ad603888e7ac1553
Ego Lydia <94656569+EgoLydia@users.noreply.github.com>
2022-02-27 18:54:27 +0100

    Feature: Add decrement and previous function

--- script.js
+++ script.js
@@ -7,15 +7,28 @@ var images = [
     "img/solduc2.jpg",
     "img/truck4scale.jpg",
  ]
+
  var currentIndex= 0;
+
  var img = document.querySelector('img');
  var next = document.getElementById('next-button');
+ var prev = document.getElementById('prev-button');
+
  var incrementIndex = function(){
      currentIndex = currentIndex +1;
      if(currentIndex > images.length - 1){currentIndex = 0}
      return currentIndex;
  }
+
+ var decrementIndex = function(){
+     currentIndex = currentIndex -1;
+     if(currentIndex < 0 ){currentIndex=images.length-1}
+     return currentIndex;
+ }
  next.onclick = function(){
      console.log(currentIndex);
     img.setAttribute('src', images[incrementIndex(currentIndex)])
+ }
+ prev.onclick = function(){
+    img.setAttribute('src', images[decrementIndex(currentIndex)] )
  }
