0e76d5c2d4edff93c8e9e19c06bc4a30ca04e1c5
Ego Lydia <94656569+EgoLydia@users.noreply.github.com>
2022-02-27 18:53:55 +0100

    feature: Add increment and next function

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ var images = [
     "img/solduc2.jpg",
     "img/truck4scale.jpg",
  ]
+ var currentIndex= 0;
+ var img = document.querySelector('img');
+ var next = document.getElementById('next-button');
+ var incrementIndex = function(){
+     currentIndex = currentIndex +1;
+     if(currentIndex > images.length - 1){currentIndex = 0}
+     return currentIndex;
+ }
+ next.onclick = function(){
+     console.log(currentIndex);
+    img.setAttribute('src', images[incrementIndex(currentIndex)])
+ }
